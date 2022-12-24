eb9d7bc156becfecc29749014651c5023a99cbb4
d36choi <yuio1126@gmail.com>
2022-12-24 16:00:36 +0900

    feat: EnableAutoConfiguration 추가

--- src/main/java/com/api/cossc/CosscApplication.java
+++ src/main/java/com/api/cossc/CosscApplication.java
@@ -1,9 +1,13 @@
 package com.api.cossc;
 
 import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.autoconfigure.jdbc.DataSourceAutoConfiguration;
 
 @SpringBootApplication
+@EnableAutoConfiguration(exclude = {DataSourceAutoConfiguration.class})
+
 public class CosscApplication {
 
 	public static void main(String[] args) {
