f68184a408688ba60eccc86b5063e40ea2a4d0ae
d36choi <yuio1126@gmail.com>
2022-12-25 23:08:20 +0900

    fix: Repository annotation & 불필요한 config 제거

--- src/main/java/com/api/cossc/CosscApplication.java
+++ src/main/java/com/api/cossc/CosscApplication.java
@@ -6,8 +6,6 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.autoconfigure.jdbc.DataSourceAutoConfiguration;
 
 @SpringBootApplication
-@EnableAutoConfiguration(exclude = {DataSourceAutoConfiguration.class})
-
 public class CosscApplication {
 
 	public static void main(String[] args) {
