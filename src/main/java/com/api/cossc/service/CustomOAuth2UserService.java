78c3fa6f1d3d80f23285631406c41058e2a35d20
d36choi <yuio1126@gmail.com>
2022-12-31 00:00:24 +0900

    feat: github oauth2 user 구현 및 user key column 추가
    - 기존 로직은 google 기준, github는 구현되어있지 않아 깃허브 로그인시 오류 발생 (github email attr 문제)
    
    close issue #2

--- src/main/java/com/api/cossc/service/CustomOAuth2UserService.java
+++ src/main/java/com/api/cossc/service/CustomOAuth2UserService.java
@@ -36,32 +36,34 @@ public class CustomOAuth2UserService extends DefaultOAuth2UserService {
   // 획득한 유저정보를 Java Model과 맵핑하고 프로세스 진행
   private OAuth2User process(OAuth2UserRequest oAuth2UserRequest, OAuth2User oAuth2User) {
     AuthProvider authProvider = AuthProvider.valueOf(oAuth2UserRequest.getClientRegistration().getRegistrationId().toUpperCase());
-    OAuth2UserInfo userInfo = OAuth2UserInfoFactory.getOAuth2UserInfo(authProvider, oAuth2User.getAttributes());
+    OAuth2UserInfo userInfo = OAuth2UserInfoFactory.getOAuth2UserInfo(authProvider, oAuth2User);
 
-    if (userInfo.getEmail().isEmpty()) {
-      throw new OAuthProcessingException("Email not found from OAuth2 provider");
-    }
-    Optional<User> userOptional = userRepository.findByEmail(userInfo.getEmail());
+//    if (userInfo.getEmail().isEmpty()) {
+//      throw new OAuthProcessingException("Email not found from OAuth2 provider");
+//    }
+    String oauthKey = userInfo.getKey();
+
+    Optional<User> userOptional = userRepository.findByOauthKeyOrEmail(oauthKey, userInfo.getEmail());
     User user;
 
-    if (userOptional.isPresent()) {		// 이미 가입된 경우
+    if (userOptional.isPresent()) {
       user = userOptional.get();
       if (authProvider != user.getAuthProvider()) {
         throw new OAuthProcessingException("Wrong Match Auth Provider");
       }
 
-    } else {			// 가입되지 않은 경우
-      user = createUser(userInfo, authProvider);
+    } else {
+      user = createUser(userInfo, oauthKey, authProvider);
     }
     return CustomUserDetails.create(user, oAuth2User.getAttributes());
   }
 
-  private User createUser(OAuth2UserInfo userInfo, AuthProvider authProvider) {
+  private User createUser(OAuth2UserInfo userInfo, String oauthKey, AuthProvider authProvider) {
     User user = User.builder()
         .email(userInfo.getEmail())
         .img(userInfo.getImageUrl())
         .role(UserRole.USER)
-//        .state(UserState.ACT)
+        .oauthKey(oauthKey)
         .authProvider(authProvider)
         .build();
     return userRepository.save(user);
