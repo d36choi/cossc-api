9bf6367bf0a58393e559e8cfede809384d185eee
d36choi <yuio1126@gmail.com>
2023-02-02 22:52:28 +0900

    feat: answer 조회 api & MapStruct 세팅 추가

--- src/main/java/com/api/cossc/dto/answer/AnswerResponse.java
+++ src/main/java/com/api/cossc/dto/answer/AnswerResponse.java
@@ -1,4 +1,33 @@
 package com.api.cossc.dto.answer;
 
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+import java.util.List;
+
+@Getter
+@NoArgsConstructor
 public class AnswerResponse {
+
+    List<AnswerItem> answerItems;
+
+    private AnswerResponse(List<AnswerItem> answerItems) {
+        this.answerItems = answerItems;
+    }
+
+    public static AnswerResponse of(List<AnswerItem> answerItemList) {
+        return new AnswerResponse(answerItemList);
+    }
+
+    @Setter
+    @Getter
+    @NoArgsConstructor
+    public static class AnswerItem {
+
+        private Long id;
+        private String text;
+        private String createdBy;
+        private String updatedBy;
+    }
 }
