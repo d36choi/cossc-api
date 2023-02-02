a180eb72530c38037c202b238836f3fb7641e1c9
d36choi <yuio1126@gmail.com>
2023-02-02 22:52:28 +0900

    feat: answer 조회 api & MapStruct 세팅 추가

--- src/main/java/com/api/cossc/dto/answer/AnswerRequest.java
+++ src/main/java/com/api/cossc/dto/answer/AnswerRequest.java
@@ -1,4 +1,15 @@
 package com.api.cossc.dto.answer;
 
+import java.util.List;
+
 public class AnswerRequest {
+    private List<Long> ids;
+
+    public List<Long> getIds() {
+        return ids;
+    }
+
+    public void setIds(List<Long> ids) {
+        this.ids = ids;
+    }
 }
---
+++ src/main/java/com/api/cossc/service/answer/AnswerServiceImpl.java
@@ -0,0 +1,33 @@
+package com.api.cossc.service.answer;
+
+import com.api.cossc.converter.answer.AnswerMapper;
+import com.api.cossc.domain.AnswerEntity;
+import com.api.cossc.dto.answer.*;
+import com.api.cossc.repository.answer.AnswerRepository;
+import lombok.RequiredArgsConstructor;
+import org.springframework.stereotype.Service;
+
+import java.util.List;
+
+@RequiredArgsConstructor
+@Service
+public class AnswerServiceImpl implements AnswerService {
+
+    private final AnswerRepository answerRepository;
+    @Override
+    public AnswerResponse getAnswer(AnswerRequest answerRequest) {
+
+        List<AnswerEntity> answers = answerRepository.findAllById(answerRequest.getIds());
+        return AnswerResponse.of(answers.stream().map(AnswerMapper.INSTANCE::toAnswerItem).toList());
+    }
+
+    @Override
+    public AnswerCreationResponse create(AnswerCreationRequest answerCreationRequest) {
+        return null;
+    }
+
+    @Override
+    public AnswerDeletionResponse delete(AnswerDeletionRequest answerDeletionRequest) {
+        return null;
+    }
+}
