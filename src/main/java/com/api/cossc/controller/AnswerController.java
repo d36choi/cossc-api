583496ac98220909d5339e151fc9654eae73a29a
d36choi <yuio1126@gmail.com>
2023-02-02 23:39:52 +0900

    feat: answer 생성 test 추가
    - mapstruct testAnnotationProcessor 추가
    - mapstruct clean -> build 시 Impl 생성
    - AnswerEntity 는 setter 로 mapstruct mapping 하면 안되므로 Builder 를 강제 (constructor 를 Protected범위로)

--- src/main/java/com/api/cossc/controller/AnswerController.java
+++ src/main/java/com/api/cossc/controller/AnswerController.java
@@ -3,6 +3,7 @@ package com.api.cossc.controller;
 import com.api.cossc.dto.answer.*;
 import com.api.cossc.service.answer.AnswerService;
 import lombok.RequiredArgsConstructor;
+import org.springframework.security.access.annotation.Secured;
 import org.springframework.validation.annotation.Validated;
 import org.springframework.web.bind.annotation.*;
 
@@ -13,13 +14,11 @@ public class AnswerController {
 
     private final AnswerService answerService;
 
-
     @GetMapping("/answer")
     public AnswerResponse getAnswer(final AnswerRequest answerRequest) {
 
         return answerService.getAnswer(answerRequest);
     }
-
 
     @PostMapping("/answer/create")
     public AnswerCreationResponse create(final AnswerCreationRequest answerCreationRequest) {
@@ -27,12 +26,14 @@ public class AnswerController {
         return answerService.create(answerCreationRequest);
     }
 
+    @Secured("ROLE_ADMIN")
     @PatchMapping("/answer/update")
     public AnswerCreationResponse update(final AnswerCreationRequest answerCreationRequest) {
 
         return answerService.create(answerCreationRequest);
     }
 
+    @Secured("ROLE_ADMIN")
     @DeleteMapping("/answer/delete")
     public AnswerDeletionResponse delete(final AnswerDeletionRequest answerDeletionRequest) {
 
--- src/main/java/com/api/cossc/converter/answer/AnswerMapper.java
+++ src/main/java/com/api/cossc/converter/answer/AnswerMapper.java
@@ -1,6 +1,7 @@
 package com.api.cossc.converter.answer;
 
 import com.api.cossc.domain.AnswerEntity;
+import com.api.cossc.dto.answer.AnswerCreationRequest;
 import com.api.cossc.dto.answer.AnswerResponse;
 import org.mapstruct.Mapper;
 import org.mapstruct.factory.Mappers;
@@ -10,4 +11,7 @@ public interface AnswerMapper {
 
     AnswerMapper INSTANCE = Mappers.getMapper(AnswerMapper.class);
     AnswerResponse.AnswerItem toAnswerItem(AnswerEntity answer);
+
+
+    AnswerEntity toAnswerEntity(AnswerCreationRequest answerCreationRequest);
 }
