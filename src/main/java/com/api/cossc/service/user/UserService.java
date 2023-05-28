9058bb5bb4c5b7d7932c0982efc57a860960d99d
Marc Lee <chlee1252@gmail.com>
2023-05-29 04:12:23 +0900

    feat: user에 tag 추가 API 개발

--- src/main/java/com/api/cossc/service/user/UserService.java
+++ src/main/java/com/api/cossc/service/user/UserService.java
@@ -1,6 +1,7 @@
 package com.api.cossc.service.user;
 
 
+import com.api.cossc.dto.tag.TagRequest;
 import com.api.cossc.dto.user.UserMainResponse;
 import org.springframework.security.core.userdetails.UserDetails;
 
@@ -8,4 +9,6 @@ public interface UserService {
 
 
     UserMainResponse getUserMe(UserDetails userDetails) throws Exception;
+
+    boolean insertUserTag(UserDetails userDetails, TagRequest tagRequest) throws Exception;
 }
