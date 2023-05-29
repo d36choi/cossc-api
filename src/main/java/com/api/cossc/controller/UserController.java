b3ae1d71757050d0dcedcb9962cea6476e668808
Changhwan (Marc) Lee <45862410+chlee1252@users.noreply.github.com>
2023-05-29 22:18:20 +0900

    user tag 추가 (#13)
    * feat: user에 tag 추가 API 개발
    
    * feat: 불필요 import 제거

--- src/main/java/com/api/cossc/controller/UserController.java
+++ src/main/java/com/api/cossc/controller/UserController.java
@@ -1,13 +1,17 @@
 package com.api.cossc.controller;
 
+import com.api.cossc.dto.tag.TagRequest;
 import com.api.cossc.dto.user.UserMainResponse;
 import com.api.cossc.security.CustomUserDetails;
 import com.api.cossc.service.user.UserService;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
+import org.springframework.http.ResponseEntity;
 import org.springframework.security.access.prepost.PreAuthorize;
 import org.springframework.security.core.annotation.AuthenticationPrincipal;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RestController;
 
 @Slf4j
@@ -21,5 +25,11 @@ public class UserController {
   @PreAuthorize("hasAnyRole('ROLE_USER','ROLE_ADMIN')")
   public UserMainResponse getCurrentUser(@AuthenticationPrincipal CustomUserDetails user) throws Exception {
     return userService.getUserMe(user);
+  }
+
+  @PostMapping("/user/tag")
+  @PreAuthorize("hasAnyRole('ROLE_USER','ROLE_ADMIN')")
+  public ResponseEntity<Boolean> insertUserTag(@AuthenticationPrincipal CustomUserDetails user, @RequestBody TagRequest tagRequest) throws Exception {
+    return ResponseEntity.ok(userService.insertUserTag(user, tagRequest));
   }
 }
