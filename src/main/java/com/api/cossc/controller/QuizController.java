e4097895b83b37f3dab7cc9e0e626c88066e32c3
Marc Lee <chlee1252@gmail.com>
2023-06-05 20:55:05 +0900

    feat: daily quiz API에서 Spring Security로 유저 정보 전달로 변경

--- src/main/java/com/api/cossc/controller/QuizController.java
+++ src/main/java/com/api/cossc/controller/QuizController.java
@@ -1,8 +1,11 @@
 package com.api.cossc.controller;
 
 import com.api.cossc.dto.quiz.*;
+import com.api.cossc.security.CustomUserDetails;
 import com.api.cossc.service.quiz.QuizService;
 import lombok.RequiredArgsConstructor;
+import org.springframework.security.access.prepost.PreAuthorize;
+import org.springframework.security.core.annotation.AuthenticationPrincipal;
 import org.springframework.validation.annotation.Validated;
 import org.springframework.web.bind.annotation.*;
 
@@ -17,9 +20,10 @@ public class QuizController {
 
 
     @GetMapping("/quiz/daily")
-    public DailyQuizResponse getDailyQuiz(DailyQuizRequest dailyQuizRequest) {
+    @PreAuthorize("hasAnyRole('ROLE_USER','ROLE_ADMIN')")
+    public DailyQuizResponse getDailyQuiz(@AuthenticationPrincipal CustomUserDetails user) {
 
-        return quizService.getDailyQuiz(dailyQuizRequest);
+        return quizService.getDailyQuiz(user);
     }
     @PostMapping("/quiz/create")
     public QuizCreationResponse create(@RequestBody QuizCreationRequest quizCreationRequest) {
