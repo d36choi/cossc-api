04739a71be1b3bf9f98860541247e1282e13bd88
Marc Lee <chlee1252@gmail.com>
2023-06-05 21:01:23 +0900

    fix: Getter 어노테이션 제거

--- src/main/java/com/api/cossc/dto/quiz/DailyQuizResponse.java
+++ src/main/java/com/api/cossc/dto/quiz/DailyQuizResponse.java
@@ -1,11 +1,9 @@
 package com.api.cossc.dto.quiz;
 
 import lombok.Builder;
-import lombok.Getter;
 
 import java.util.List;
 
-@Getter
 public record DailyQuizResponse(List<QuizResponse> quizResponses) {
 
     @Builder
